060d1ed75298c0f1d0a7ea5976b56a91ad01f63e
popkik820 <481257084@qq.com>
2025-09-22 22:14:41 +0800

    Add files via upload

---
+++ ServoSpin.ino
@@ -0,0 +1,28 @@
+#include<Servo.h>
+Servo myservo; 
+int degree;
+
+void setup() {
+  // put your setup code here, to run once:
+ myservo.attach(9);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+degree = 30;
+myservo.write(degree);
+delay(1000);  
+
+degree = 45;
+myservo.write(degree);
+delay(1000);
+
+degree = 90;
+myservo.write(degree);
+delay(1000);
+
+degree = 135;
+myservo.write(degree);
+delay(1000); 
+
+}
